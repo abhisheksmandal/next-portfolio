dd8e7b3155285f89ba6d1b334a1f99fca6d0e130
Abhishek Mandal <official.abhishek.mandal@gmail.com>
2025-07-24 18:22:50 +0000

    I want it to be in circular design

--- src/components/go-to-top.tsx
+++ src/components/go-to-top.tsx
@@ -38,7 +38,7 @@ export function GoToTop() {
       size="icon"
       onClick={scrollToTop}
       className={cn(
-        "fixed bottom-8 right-8 z-50 transition-opacity duration-300",
+        "fixed bottom-8 right-8 z-50 rounded-full transition-opacity duration-300",
         isVisible ? "opacity-100" : "opacity-0 pointer-events-none"
       )}
       aria-label="Go to top"
